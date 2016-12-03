24ddbc97a39e8cac317a0dacf8f031e176744ba1
Tsinat Zeree <cheru87@gmail.com>
2016-12-02 16:19:43 -0800

    added css and event handler to images

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
 import data from './data';
 
 const root = document.getElementById('app');
-const container = document.createElement('div');
 const images = data.images.map((image, index) => {
     const myDiv = document.createElement('div');
-    myDiv.setAttribute("height", "168");
-    myDiv.setAttribute("width", "324");
-    myDiv.style.border = "1px solid grey";
     myDiv.className = "item";
     myDiv.setAttribute("id", `item${index}`);
     const elem = document.createElement('img');
     elem.setAttribute("src", image.url);
     elem.setAttribute("height", "168");
-    elem.setAttribute("width", "324");
+    elem.setAttribute("width", "100%");
     elem.style.padding = "10px";
     myDiv.appendChild(elem);
     return myDiv;
@@ -22,8 +18,8 @@ const docFrag = document.createDocumentFragment();
 for(let i = 0; i < images.length; i++) {
     docFrag.appendChild(images[i]);
 }
-container.appendChild(docFrag);
-root.appendChild(container);
+
+root.appendChild(docFrag);
 
 for(let i = 0; i < 3; i++) {
     addListener(`item${i}`);
