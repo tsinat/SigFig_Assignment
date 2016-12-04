4911b526fdefe8dbb1f3a39a12519b2c3e1b90d9
Tsinat Zeree <cheru87@gmail.com>
2016-12-03 23:25:25 -0800

    added event listeners to the pictures

--- js/index.js
+++ js/index.js
@@ -1,31 +1,88 @@
 import data from './data';
 
-const root = document.getElementById('app');
-const images = data.images.map((image, index) => {
-    const myDiv = document.createElement('div');
-    myDiv.className = "item";
-    myDiv.setAttribute("id", `item${index}`);
-    const elem = document.createElement('img');
-    elem.setAttribute("src", image.url);
-    elem.setAttribute("height", "168");
-    elem.setAttribute("width", "100%");
-    elem.style.padding = "10px";
-    myDiv.appendChild(elem);
-    return myDiv;
-});
+(function() {
+    'use strict';
 
-const docFrag = document.createDocumentFragment();
-for(let i = 0; i < images.length; i++) {
-    docFrag.appendChild(images[i]);
-}
+    const root = document.getElementById('app');
+    const images = data.images.map((image, index) => {
+        const myDiv = document.createElement('div');
+        myDiv.className = "item";
+        myDiv.setAttribute("draggable", true);
+        const newImg = document.createElement('img');
+        newImg.setAttribute("src", image.url);
+        newImg.setAttribute("width", "100%");
+        newImg.setAttribute("height", "auto");
+        newImg.style.padding = "10px";
+        myDiv.appendChild(newImg);
+        return myDiv;
+    });
 
-root.appendChild(docFrag);
+    const docFrag = document.createDocumentFragment();
+    for(let i = 0; i < images.length; i++) {
+        docFrag.appendChild(images[i]);
+    }
+    root.appendChild(docFrag);
+    const draggableDiv = document.querySelectorAll('.item');
 
-for(let i = 0; i < 3; i++) {
-    addListener(`item${i}`);
-}
-function addListener(item) {
-    document.getElementById(`${item}`).addEventListener('click', function(e) {
-        console.log('Yeah it is working!!', e.target);
+
+    function handleDragStart(e) {
+      this.style.opacity = '0.4';  // this / e.target is the source node.
+    }
+
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault(); // Necessary. Allows us to drop.
+      }
+
+      e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+      return false;
+    }
+
+    function handleDragEnter(e) {
+      // this / e.target is the current hover target.
+      this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+      this.classList.remove('over');  // this / e.target is previous target element.
+    }
+    function handleDrop(e) {
+        // this / e.target is current target element.
+
+        if (e.stopPropagation) {
+            e.stopPropagation(); // stops the browser from redirecting.
+        }
+
+        // See the section on the DataTransfer object.
+
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        // this/e.target is the source node.
+
+        [].forEach.call(draggableDiv, function (col) {
+            col.classList.remove('over');
+    });
+    }
+
+    //
+    const addListener = (item) => {
+        item.addEventListener('dragstart', function(e) {
+            this.style.opacity = '0.5';
+            console.log('yeah');
+        })
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+    }
+
+
+    draggableDiv.forEach((item) => {
+        addListener(item);
     })
-}
+})();
