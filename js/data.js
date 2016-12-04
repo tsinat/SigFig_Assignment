8c997fe6d0aeb4d3bb51210bb5bd34f790b483b7
Tsinat Zeree <cheru87@gmail.com>
2016-12-04 15:42:19 -0800

    functional drag and drop image gallery

--- js/data.js
+++ js/data.js
@@ -2,7 +2,12 @@ const data = {
     images: [
         {"url": "http://cdn.skilledup.com/wp-content/uploads/2014/11/graphic-design-tools-Feature_1290x688_MS.jpg"},
         {"url": "https://content-static.upwork.com/blog/uploads/sites/4/2014/03/web-development-cropped.jpg"},
+        {"url": "https://content-static.upwork.com/blog/uploads/sites/4/2014/03/web-development-cropped.jpg"},
         {"url": "http://mliesl.edu/images/IMG_1464.jpg"},
+        {"url": "http://cdn.skilledup.com/wp-content/uploads/2014/11/graphic-design-tools-Feature_1290x688_MS.jpg"},
+        {"url": "https://content-static.upwork.com/blog/uploads/sites/4/2014/03/web-development-cropped.jpg"},
+        {"url": "http://mliesl.edu/images/IMG_1464.jpg"},
+        {"url": "https://www.ccny.cuny.edu/sites/default/files/styles/top_slider/public/cintbanner.jpg"},
         {"url": "https://www.ccny.cuny.edu/sites/default/files/styles/top_slider/public/cintbanner.jpg"}
     ]
 };
--- js/index.js
+++ js/index.js
@@ -7,80 +7,86 @@ import data from './data';
     const images = data.images.map((image, index) => {
         const myDiv = document.createElement('div');
         myDiv.className = "item";
+        myDiv.classList.add("col-xs-3");
         myDiv.setAttribute("draggable", true);
+        myDiv.setAttribute("margin", '10');
         const newImg = document.createElement('img');
         newImg.setAttribute("src", image.url);
         newImg.setAttribute("width", "100%");
-        newImg.setAttribute("height", "auto");
+        newImg.setAttribute("height", "200px");
         newImg.style.padding = "10px";
         myDiv.appendChild(newImg);
         return myDiv;
     });
 
     const docFrag = document.createDocumentFragment();
+
     for(let i = 0; i < images.length; i++) {
         docFrag.appendChild(images[i]);
     }
-    root.appendChild(docFrag);
+    const wrapper = document.createElement('div');
+    wrapper.classList.add("container");
+
+    wrapper.appendChild(docFrag);
+    root.appendChild(wrapper);
     const draggableDiv = document.querySelectorAll('.item');
 
 
+    let dragSrcEl = null;
     function handleDragStart(e) {
-      this.style.opacity = '0.4';  // this / e.target is the source node.
+      this.style.opacity = '0.4';
+
+      dragSrcEl = this;
+
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
     }
 
     function handleDragOver(e) {
       if (e.preventDefault) {
-        e.preventDefault(); // Necessary. Allows us to drop.
+        e.preventDefault();
       }
 
-      e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
+      e.dataTransfer.dropEffect = 'move';
       return false;
     }
 
-    function handleDragEnter(e) {
-      // this / e.target is the current hover target.
-      this.classList.add('over');
-    }
+    // function handleDragEnter(e) {
+    //   this.classList.add('over');
+    // }
+    //
+    // function handleDragLeave(e) {
+    //   this.classList.remove('over');
+    // }
 
-    function handleDragLeave(e) {
-      this.classList.remove('over');  // this / e.target is previous target element.
-    }
     function handleDrop(e) {
-        // this / e.target is current target element.
-
         if (e.stopPropagation) {
-            e.stopPropagation(); // stops the browser from redirecting.
+            e.stopPropagation();
         }
-
-        // See the section on the DataTransfer object.
+        // Don't do anything if dropping the same column we're dragging.
+         if (dragSrcEl != this) {
+           // Set the source column's HTML to the HTML of the column we dropped on.
+           dragSrcEl.innerHTML = this.innerHTML;
+           this.innerHTML = e.dataTransfer.getData('text/html');
+         }
 
         return false;
     }
 
     function handleDragEnd(e) {
-        // this/e.target is the source node.
-
         [].forEach.call(draggableDiv, function (col) {
             col.classList.remove('over');
-    });
+        });
     }
 
-    //
     const addListener = (item) => {
-        item.addEventListener('dragstart', function(e) {
-            this.style.opacity = '0.5';
-            console.log('yeah');
-        })
-        item.addEventListener('dragstart', handleDragStart, false);
-        item.addEventListener('dragenter', handleDragEnter, false);
-        item.addEventListener('dragover', handleDragOver, false);
-        item.addEventListener('dragleave', handleDragLeave, false);
-        item.addEventListener('drop', handleDrop, false);
-        item.addEventListener('dragend', handleDragEnd, false);
+        item.addEventListener('dragstart', handleDragStart);
+        // item.addEventListener('dragenter', handleDragEnter);
+        item.addEventListener('dragover', handleDragOver);
+        // item.addEventListener('dragleave', handleDragLeave);
+        item.addEventListener('drop', handleDrop);
+        item.addEventListener('dragend', handleDragEnd);
     }
-
 
     draggableDiv.forEach((item) => {
         addListener(item);
